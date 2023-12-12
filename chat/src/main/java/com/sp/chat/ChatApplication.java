687e35f229ede450b2ecf96b672c7ada5789395d
Ugina <86170855+SickSect@users.noreply.github.com>
2023-12-12 22:37:28 +0300

    init

---
+++ chat/src/main/java/com/sp/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.sp.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
